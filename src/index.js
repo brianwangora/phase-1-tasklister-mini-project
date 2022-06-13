47bc9c504ee8185b03e41e25c6d870b4ea44a74c
Brian Wangora <brianwangora@gmail.com>
2022-06-13 18:10:23 +0300

    Task lister done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTaskDescription(e.target.newTaskDescription.value)
+    form.reset()
+  })
 });
+
+function createTaskDescription (taskDescription, priorities) {
+  let p = document.createElement('p')
+  let bttn = document.createElement('button')
+  bttn.addEventListener('click', handleDelete)
+  bttn.textContent = 'x'
+  p.textContent = `${taskDescription} `
+  p.appendChild(bttn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
